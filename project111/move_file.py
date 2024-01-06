15f4cd9c8a3f7210e6d14d100c6097eb1c358b40
Fathima Siddiqua <61356106+Fathima2020@users.noreply.github.com>
2024-01-06 18:12:40 +0530

    Add files via upload

---
+++ project111/move_file.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+print(dir(os))
+print(os.getcwd)
+from_dir = "C:/Users/HAJEERA RUBAI/Desktop/project111/move_file.py"
+to_dir = "C:/Users/HAJEERA RUBAI/Desktop/project111/documents_file"
+from_dir = "C:/Users/HAJEERA RUBAI/Desktop/project111/move_file.py"
+list_of_files = os.listdir(from_dir)
+list_of_files=os.listdir(from_dir)
+print(list_of_files)
